ec862afff70eee509dcb22f5de05b903dfa3d0f9
Philippe Pitz Clairoux <philippe.pitz.clairoux@usherbrooke.ca>
2023-05-05 21:21:18 -0400

    Add simple string filtering

--- PacketSniffer.go
+++ PacketSniffer.go
@@ -1,26 +1,33 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/pcap"
 	"log"
 	"math"
+	"os"
 	"packet-sniffer/pkg/goprettypackets"
 	"packet-sniffer/pkg/goresolve"
+	"strings"
 )
 
 var (
 	deviceName         = flag.String("interface", "enp0s31f6", "interface name (ip addr)")
 	showLayers         = flag.Bool("show-layers", true, "display layers and their content")
 	resolveIpAddresses = flag.Bool("resolve-ip", true, "try finding host name if possible")
+	websiteFilter      = flag.String("website-filter", "", "Only show packets that have this website")
 )
 
 func main() {
 	flag.Parse()
 	packets := make(chan gopacket.Packet)
 	defer close(packets)
+
+	fmt.Println("Got the following values : ", *deviceName, *showLayers, *resolveIpAddresses, *websiteFilter)
+	bufio.NewReader(os.Stdin).ReadLine()
 
 	listener, err := pcap.OpenLive(*deviceName, math.MaxInt32, true, pcap.BlockForever)
 
@@ -39,29 +46,41 @@ func main() {
 		flow := networkLayer.NetworkFlow()
 
 		go func(packet gopacket.Packet, flow gopacket.Flow) {
-			var srcString string
-			var dstString string
+			var srcString = flow.Src().String()
+			var dstString = flow.Dst().String()
+			var displayPacket = len(*websiteFilter) > 0
+			resolveIpAddress(&displayPacket, &srcString, &dstString)
 
-			if *resolveIpAddresses {
-				srcString = fmt.Sprintf("(%s) %v", flow.Src(), goresolve.Ip(flow.Src().String()))
-				dstString = fmt.Sprintf("(%s) %v", flow.Dst(), goresolve.Ip(flow.Dst().String()))
-			} else {
-				srcString = flow.Src().String()
-				dstString = flow.Dst().String()
-			}
+			if (len(*websiteFilter) > 0 && displayPacket) || len(*websiteFilter) == 0 {
+				fmt.Printf("Got a packet : source => %s , destination => %s\n", srcString, dstString)
 
-			fmt.Printf("Got a packet : source => %s , destination => %s\n", srcString, dstString)
-
-			if *showLayers {
-				for _, layer := range packet.Layers() {
-					displayLayer(&layer, packet)
+				if *showLayers {
+					for _, layer := range packet.Layers() {
+						displayLayer(&layer)
+					}
 				}
 			}
 		}(packet, flow)
 	}
 }
 
-func displayLayer(layer *gopacket.Layer, packet gopacket.Packet) {
+func resolveIpAddress(displayPacket *bool, srcString *string, dstString *string) {
+	if *resolveIpAddresses || *displayPacket {
+		src := goresolve.Ip(*srcString)
+		dst := goresolve.Ip(*dstString)
+
+		if strings.Contains(src[0], *websiteFilter) || strings.Contains(dst[0], *websiteFilter) {
+			*displayPacket = true
+		} else {
+			*displayPacket = false
+		}
+
+		*srcString = fmt.Sprintf("(%s) %v", *srcString, src)
+		*dstString = fmt.Sprintf("(%s) %v", *dstString, dst)
+	}
+}
+
+func displayLayer(layer *gopacket.Layer) {
 	layerType := (*layer).LayerType()
 	layerContent := (*layer).LayerContents()
 	layerPayload := (*layer).LayerPayload()
--- go.mod
+++ go.mod
@@ -2,8 +2,6 @@ module packet-sniffer
 
 go 1.20
 
-require (
-	github.com/google/gopacket v1.1.19
-)
+require github.com/google/gopacket v1.1.19
 
 require golang.org/x/sys v0.0.0-20190412213103-97732733099d // indirect
